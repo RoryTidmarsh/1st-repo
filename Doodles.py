8b11e1d5f12383792a4e6459f83f08aaef34b200
RoryTidmarsh <151524572+RoryTidmarsh@users.noreply.github.com>
2023-11-21 12:24:27 +0000

    Update Doodles.py
    changed dog speed - remember doodles is rapid

--- Doodles.py
+++ Doodles.py
@@ -1,6 +1,6 @@
 print('doodles is a smelly dog')
 
-v = 5 #ms-1
+v = 7.5 #ms-1
 vmax = 13.411 #ms-1
 print(f'Doodles can run at {v}ms-1')
 print(f'Doodles runs {vmax- v}ms-1 slower than the fastest border collie')
